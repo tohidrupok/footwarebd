77c3d310cafa77c80da1abd4a95cb64cdc77c52c
tohidrupok <tohidrupok@gmail.com>
2024-04-30 17:37:52 +0600

    service / supply chain bakckend all are done

--- service/admin.py
+++ service/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from .models import Publication, Category, Company
 
-# Register your models here.
-from django.contrib import admin
-from .models import Publication
-
-@admin.register(Publication)
 class PublicationAdmin(admin.ModelAdmin):
     list_display = ('name', 'publication_date')
     search_fields = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin): 
+    list_display = ['category_name']     
+    prepopulated_fields = {'slug': ('category_name',)}  # Note the tuple ('category_name',)
+
+admin.site.register(Publication, PublicationAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Company)
--- service/models.py
+++ service/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+#Resource library
+#
 class Publication(models.Model):
     publication_date = models.DateField()
     name = models.CharField(max_length=200)
@@ -7,3 +8,28 @@ class Publication(models.Model):
 
     def __str__(self):
         return self.name
+
+
+
+#supply chain
+
+class Category(models.Model):
+    category_name = models.CharField(max_length = 50, unique = True)
+    slug = models.SlugField(max_length=200, unique=True)
+   
+    def __str__(self):
+        return self.category_name 
+    
+
+class Company(models.Model):
+    serial_no = models.IntegerField(unique=True)
+    title = models.ForeignKey(Category, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
+    contact = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    remark = models.TextField()
+    extra = models.CharField(max_length=100, null= True, blank= True)
+
+    def __str__(self):
+        return self.title.category_name + ' - ' + self.company_name
