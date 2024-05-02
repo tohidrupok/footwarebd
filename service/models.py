fdc76c79e450f876157698c1e986802abef825e9
tohidrupok <tohidrupok@gmail.com>
2024-05-02 10:52:38 +0600

    supp chain all page linked

--- service/models.py
+++ service/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 #Resource library
-#
 class Publication(models.Model):
     publication_date = models.DateField()
     name = models.CharField(max_length=200)
@@ -8,11 +7,7 @@ class Publication(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
 #supply chain
-
 class Category(models.Model):
     category_name = models.CharField(max_length = 50, unique = True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -20,7 +15,6 @@ class Category(models.Model):
     def __str__(self):
         return self.category_name 
     
-
 class Company(models.Model):
     serial_no = models.IntegerField(unique=True)  #delet this fields
     title = models.ForeignKey(Category, on_delete=models.CASCADE)
