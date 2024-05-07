141c94222e92c1bf56d5c1a69219b97fe47fdbe4
tohidrupok <tohidrupok@gmail.com>
2024-05-07 14:23:23 +0600

    bloag page create

--- vlogs/urls.py
+++ vlogs/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
 
 ]
--- vlogs/views.py
+++ vlogs/views.py
@@ -13,3 +13,8 @@ def contact(request):
 def about(request):
 
     return render(request, 'about.html')
+
+def blog(request):
+    
+
+    return render(request, 'blog.html')
